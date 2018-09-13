99f188cc0f2da11735e5bc1797f35699101d4222
c10t <c10t@fa.ke>
2018-09-13 09:22:34 +0900

    add sample spiders

---
+++ engine/concept/concept/spiders/quotes_spider.py
@@ -0,0 +1,34 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        # page = response.url.split("/")[-2]
+        # filename = f'quotes-{page}.html'
+        # with open(filename, mode='wb') as f:
+        #    f.write(response.body)
+        #
+        # self.log(f'saved file {filename}')
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').extract_first(),
+                'author': quote.css('small.author::text').extract_first(),
+                'tags': quote.css('div.tags a.tag::text').extract(),
+            }
+
+        # next_page = response.css('li.next a::attr(href)').extract_first()
+        # if next_page is not None:
+        #     next_page = response.urljoin(next_page)
+        #     yield scrapy.Request(next_page, callback=self.parse)
+        for href in response.css('li.next a::attr(href)'):
+            yield response.follow(href, callback=self.parse)
